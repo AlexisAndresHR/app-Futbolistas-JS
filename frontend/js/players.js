708a30fdd5e0936480ee95885f193425396f961d
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2021-08-03 20:51:52 -0500

    The players registers of the API (backend) are now listed in the interface with a fetch connection

--- frontend/js/players.js
+++ frontend/js/players.js
@@ -8,23 +8,25 @@ const teamField = document.getElementById('team');
 const newPlayerForm = document.getElementById('new-player-form');
 const registerPlayerBtn = document.getElementById('new-player-btn');
 
-let players = [
-    {
-        firstName: "Jose de Jesus",
-        lastName: "Corona",
-        position: "Goalkeeper",
-        team: "Cruz Azul FC"
-    }
-];// Initial variable for the players list
+let players = [];// Initial variable for the players list
 let action = 'Save';// Flag variable for the type of action (Save or Edit)
 
 
 /**
- * Function that puts the list of players in a table row
+ * Function that puts the list of players in a table row (now async)
  */
-function listPlayers() {
-    let htmlPlayers = players.map((player, index) =>
-        `<tr>
+async function listPlayers() {
+
+    try {
+        // Makes a request to the data API (backend) and load the real Players registers
+        const response = await fetch("http://localhost:5000/players");// Using fetch (HTTP requests)
+        const playersRegisters = await response.json();
+        if (Array.isArray(playersRegisters) && playersRegisters.length > 0) {
+            players = playersRegisters;// Assign the server registers to the local variable
+        }
+
+        const htmlPlayers = players.map((player, index) =>
+            `<tr>
             <th scope="row"> ${index} </th>
             <td> ${player.firstName} </td>
             <td> ${player.lastName} </td>
@@ -38,19 +40,24 @@ function listPlayers() {
                 </div>
             </td>
         </tr>`
-    ).join("");
-    playersTable.innerHTML = htmlPlayers;// Assign the table rows to the HTML component (with innerHTML property)
+        ).join("");
+        playersTable.innerHTML = htmlPlayers;// Assign the table rows to the HTML component (with innerHTML property)
 
-    // Assign an onclick function to each button on the players table (edit data)
-    Array.from(document.getElementsByClassName('edit-item')).forEach(
-        (editButton, index) => editButton.onclick = editPlayerData(index)
-        // closure structure to invoke the function giving the status of the button
-    );
-    // Assign an onclick function to each button on the players table (delete player)
-    Array.from(document.getElementsByClassName('delete-item')).forEach(
-        (deleteButton, index) => deleteButton.onclick = deletePlayer(index)
-        // closure structure to invoke the function giving the status of the button
-    );
+        // Assign an onclick function to each button on the players table (edit data)
+        Array.from(document.getElementsByClassName('edit-item')).forEach(
+            (editButton, index) => editButton.onclick = editPlayerData(index)
+            // closure structure to invoke the function giving the status of the button
+        );
+        // Assign an onclick function to each button on the players table (delete player)
+        Array.from(document.getElementsByClassName('delete-item')).forEach(
+            (deleteButton, index) => deleteButton.onclick = deletePlayer(index)
+            // closure structure to invoke the function giving the status of the button
+        );
+    }
+    catch (error) {
+
+    }
+
 }
 
 /**
@@ -117,6 +124,22 @@ function deletePlayer(index) {
 }
 
 listPlayers();// Executes the function
+
+/**
+ * Makes a request to the data API (backend) and load the real Players registers
+ */
+/* function loadPlayersData() {
+    // Using fetch (HTTP requests)
+    fetch("http://localhost:5000/players")
+        .then((response) => {
+            if (response.ok)
+                return response.json();// If the response is 'ok', is converted into a legible JSON
+        })
+        .then((playersRegisters) => {
+            console.log({ playersRegisters });
+        });
+} */
+
 
 newPlayerForm.onsubmit = submitPlayerData;// Prevent the default parameters sent (in URL) & redirect of the form data
 registerPlayerBtn.onclick = submitPlayerData;// Save new player (button action)
