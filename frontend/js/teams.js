4b937b01e842d80b4a8f1b5e5adc9410397ce8ad
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2021-08-06 21:03:28 -0500

    List, save, edit and delete Teams methods working and connected to the API

--- frontend/js/teams.js
+++ frontend/js/teams.js
@@ -8,23 +8,35 @@ const leagueField = document.getElementById('league');
 const newTeamForm = document.getElementById('new-team-form');
 const registerTeamBtn = document.getElementById('new-team-btn');
 
-let teams = [
-    {
-        name: "FC Barcelona",
-        country: "Spain",
-        stadium: "Camp Nou",
-        league: "La Liga"
-    }
-];// Initial variable for the Teams list
+const url = "http://localhost:5000/teams";
+
+let teams = [];// Initial variable for the Teams list
 let action = 'Save';// Flag variable for the type of action (Save or Edit)
 
 
 /**
  * Function that puts the list of teams in a table row
  */
-function listTeams() {
-    let htmlTeams = teams.map((team, index) =>
-        `<tr>
+async function listTeams() {
+    try {
+        // Makes a request to the data API (backend) to load the real Teams registers
+        const response = await fetch(url);// Using fetch (HTTP requests)
+        const teamsRegisters = await response.json();
+        if (Array.isArray(teamsRegisters)) {
+            teams = teamsRegisters;// Assign the server registers to the local variable
+        }
+
+        if (teamsRegisters.length === 0) { // If the Teams register is empty, shows an specific message in screen
+            teamsTable.innerHTML = `
+                <tr>
+                    <td colspan="5"> There are not Teams registered yet... </td>
+                </tr>
+            `;
+            return;
+        }
+
+        let htmlTeams = teams.map((team, index) =>
+            `<tr>
             <th scope="row"> ${index} </th>
             <td> ${team.name} </td>
             <td> ${team.country} </td>
@@ -38,54 +50,74 @@ function listTeams() {
                 </div>
             </td>
         </tr>`
-    ).join("");
-    teamsTable.innerHTML = htmlTeams;// Assign the table rows to the HTML component (with innerHTML property)
+        ).join("");
+        teamsTable.innerHTML = htmlTeams;// Assign the table rows to the HTML component (with innerHTML property)
 
-    // Assign an onclick function to each button on the teams table (edit data)
-    Array.from(document.getElementsByClassName('edit-item')).forEach(
-        (editButton, index) => editButton.onclick = editTeamData(index)
-        // closure structure to invoke the function giving the status of the button
-    );
-    // Assign an onclick function to each button on the teams table (delete team)
-    Array.from(document.getElementsByClassName('delete-item')).forEach(
-        (deleteButton, index) => deleteButton.onclick = deleteTeam(index)
-        // closure structure to invoke the function giving the status of the button
-    );
+        // Assign an onclick function to each button on the teams table (edit data)
+        Array.from(document.getElementsByClassName('edit-item')).forEach(
+            (editButton, index) => editButton.onclick = editTeamData(index)
+            // closure structure to invoke the function giving the status of the button
+        );
+        // Assign an onclick function to each button on the teams table (delete team)
+        Array.from(document.getElementsByClassName('delete-item')).forEach(
+            (deleteButton, index) => deleteButton.onclick = deleteTeam(index)
+            // closure structure to invoke the function giving the status of the button
+        );
+    }
+    catch (error) {
+        throw error;
+    }
 }
 
 /**
  * Save a new Team data to be listed in the table
  * @param event
  */
-function submitTeamData(event) {
+async function submitTeamData(event) {
     event.preventDefault();
-    const newRegister = {// Obtain the form fields data
-        name: nameField.value,
-        country: countryField.value,
-        stadium: stadiumField.value,
-        league: leagueField.value
-    };
-    switch (action) {
-        case 'Save':
-            teams.push(newRegister);
-            action = 'Save';
-            break;
-        case 'Edit':
-            teams[indexField.value] = newRegister;
-            action = 'Save';
-            break;
-        default:
-            // none
-            break;
-    }
-    listTeams();// Reload the function to put in screen the updated Teams list (after the new register)
+    try {
+        const newRegister = {// Obtain the form fields data
+            name: nameField.value,
+            country: countryField.value,
+            stadium: stadiumField.value,
+            league: leagueField.value
+        };
 
-    // Reset the inputs to register more data again (4 lines)
-    nameField.value = '';
-    countryField.value = '';
-    stadiumField.value = '';
-    leagueField.value = '';
-    registerTeamBtn.innerHTML = 'Create';// Reset the modal form button
+        let sendMethod = 'POST';
+        let sendUrl = url;
+        // Depending on the type of action (Save or Edit)
+        if (action === 'Save'){
+            action = 'Save';
+        }
+        else if (action === 'Edit'){
+            sendMethod = 'PUT';
+            sendUrl = `${sendUrl}/${indexField.value}`;
+            action = 'Save';
+        }
+
+        // Send the new register data to the API with fetch()
+        const response = await fetch(sendUrl, {
+            method: sendMethod,
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(newRegister)
+        });
+
+        if (response.ok){
+            listTeams();// Reload the function to put in screen the updated Teams list (after the new register)
+
+            // Reset the inputs to register more data again (4 lines)
+            nameField.value = '';
+            countryField.value = '';
+            stadiumField.value = '';
+            leagueField.value = '';
+            registerTeamBtn.innerHTML = 'Create';// Reset the modal form button
+        }
+    }
+    catch (error) {
+        throw error;
+    }
 }
 
 /**
@@ -108,11 +140,28 @@ function editTeamData(index) {
     }
 }
 
+/**
+ * Function (with async return) to delete/remove Teams registers from the data API
+ * @param index
+ * @returns {(function(): void)|*}
+ */
 function deleteTeam(index) {
-    return function clickHandler2() {
+    const sendUrl = `${url}/${index}`;// Using a JS literal string
+    return async function clickHandler2() {
         // Using array.filter, the item to be deleted is avoided and the others are added again
-        teams = teams.filter((team, teamIndex) => teamIndex !== index);
-        listTeams();// Refresh the table of Teams
+        //teams = teams.filter((team, teamIndex) => teamIndex !== index);
+        try {
+            const response = await fetch(sendUrl, {
+                method: "DELETE",
+            });// Send a DELETE request to the data API
+
+            if (response.ok){
+                listTeams();// Refresh the table of Teams
+            }
+        }
+        catch (error) {
+            throw error;
+        }
     }
 }
 
